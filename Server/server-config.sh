6a7585a5f003596adefb798231b79c082bfb04ea
Brian J Best <brian@bestmacs.com>
2024-03-03 15:51:14 -0600

    Update server-config.sh
    Update server-config.sh line 120 to change the vulnerable "chacha20-poly1305@openssh.com" to "aes256-gcm@openssh.com" to match the client side fix.  This change will need to be made to existing sshd_config files but this sets up new servers.

--- Server/server-config.sh
+++ Server/server-config.sh
@@ -117,7 +117,7 @@ echo "$serverFQDN" > /usr/local/bin/BlueSky/Server/server.txt
 echo "$serverFQDN" > /usr/local/bin/BlueSky/Admin\ Tools/server.txt
 
 ## reconfigure sshd_config to meet our specifications
-echo 'Ciphers chacha20-poly1305@openssh.com,aes256-ctr' >> /etc/ssh/sshd_config
+echo 'Ciphers aes256-gcm@openssh.com,aes256-ctr' >> /etc/ssh/sshd_config
 echo 'MACs hmac-sha2-512-etm@openssh.com,hmac-ripemd160' >> /etc/ssh/sshd_config
 sed -i '/HostKey \/etc\/ssh\/ssh_host_dsa_key/d' /etc/ssh/sshd_config
 sed -i '/HostKey \/etc\/ssh\/ssh_host_ecdsa_key/d' /etc/ssh/sshd_config
